ea70db5b96cb1f92f0f5b3a560387027a4e4f610
Nafis554998 <142597957+Nafis554998@users.noreply.github.com>
2024-03-13 21:15:41 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,384 @@
+import os
+import httpx
+from os import system as osRUB
+from os import system as cmd
+import os,time,platform
+import os, platform, time, sys
+#os.system('clear')
+#print(' [\x1b[38;5;48m●\033[1;37m]\x1b[38;5;46m CHECKING UPDATE...!\033[1;37m [\x1b[38;5;48m–\033[1;37m]\033[1;37m')
+time.sleep(2)
+os.system('clear')
+print('\033[1;97m[\x1b[38;5;48m●\033[1;37m] \x1b[38;5;50mJOIN MY PUBLIC GROUP ')
+time.sleep(0.1)
+os.system(f'xdg-open https://m.facebook.com/groups/636462365122965/?ref=share&mibextid=NSMWBT')
+os.system('clear')
+print('\x1b[38;5;46mLoding...\x1b[38;5;196m[\x1b[38;5;46mTEAM-69\x1b[38;5;196m]\x1b[38;5;46m')
+time.sleep(2)
+try:
+    import requests 
+except ImportError:
+    print('\n  installing Requests ...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n  installing futures ...\n')
+    os.system('pip install futures')
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+
+os.system('git pull');os.system('termux-setup-storage')
+try:
+    import requests
+except ImportError:
+    print('\n \033[1;91m[\033[1;93mTEAM-69\033[1;91m]\033[1;97m installing bs4 !...\n')
+    time.sleep(0.5)
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n \033[1;91m[\033[1;93mTEAM-69\033[1;91m]\033[1;97m installing futures !...\n')
+    time.sleep(0.5)
+    os.system('pip install futures')
+
+try:
+    import bs4
+except ImportError:
+    print('\n \033[1;91m[\033[1;93mTEAM-69\033[1;91m]\033[1;97m installing bs4 !...\n')
+    time.sleep(0.5)
+    os.system('pip install bs4')
+
+import os, platform, time, sys
+
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+
+print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;92mChecking For Update. . . .')
+#os.system('espeak -a 300 " Waiting for Update,"')
+time.sleep(2)
+
+
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+ print('\033[1;91m[\033[1;92m◉\033[1;91m] \033[1;92mYOU ARE 64BIT USER')
+ 
+elif bit == '32bit':
+ print('\033[1;91m[\033[1;92m◉\033[1;91m] \033[1;92mYOU ARE 32BIT USER')
+
+
+    
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as NAFISNAFIS
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit 
+model2 = requests.get('https://facebook.com/groups/1147370869269844//').text.splitlines()
+totaldmp = 0
+count = 0
+loop = 0
+oks = []
+cps = []
+id = []
+ps = []
+sid = []
+total=[]
+methods = []
+srange = 0
+saved = []
+totaldmp = 0
+filter = []    
+sim_id = ''
+android_version = subprocess.check_output('getprop ro.build.version.release',shell=True).decode('utf-8').replace('\n','')
+model = subprocess.check_output('getprop ro.product.model',shell=True).decode('utf-8').replace('\n','')
+build = subprocess.check_output('getprop ro.build.id',shell=True).decode('utf-8').replace('\n','')
+fblc = 'en_US'
+try:
+        fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')[0].replace('\n','')
+except:
+        fbcr = 'Zong'
+fbmf = subprocess.check_output('getprop ro.product.manufacturer',shell=True).decode('utf-8').replace('\n','')
+fbbd = subprocess.check_output('getprop ro.product.brand',shell=True).decode('utf-8').replace('\n','')
+fbdv = model
+fbsv = android_version
+fbca = subprocess.check_output('getprop ro.product.cpu.abilist',shell=True).decode('utf-8').replace(',',':').replace('\n','')
+fbdm = '{density=2.0,height='+subprocess.check_output('getprop ro.hwui.text_large_cache_height',shell=True).decode('utf-8').replace('\n','')+',width='+subprocess.check_output('getprop ro.hwui.text_large_cache_width',shell=True).decode('utf-8').replace('\n','')
+try:
+        fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')
+        total = 0
+        for i in fbcr:
+                total+=1
+        select = ('1','2')
+        if select == '1':
+                fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')[0].replace('\n','')
+                sim_id+=fbcr
+        elif select == '2':
+                try:
+                        fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')[1].replace('\n','')
+                        sim_id+=fbcr
+                except Exception as e:
+                        fbcr = "Zong"
+                        sim_id+=fbcr
+        else:
+                fbcr = 'Zong'
+                sim_id+=fbcr
+except:
+        fbcr = "Zong"
+device = {
+        'android_version':android_version,
+        'model':model,
+        'build':build,
+        'fblc':fblc,
+        'fbmf':fbmf,
+        'fbbd':fbbd,
+        'fbdv':model,
+        'fbsv':fbsv,
+        'fbca':fbca,
+        'fbdm':fbdm}
+
+
+S = '\033[1;37m'
+A = '\x1b[38;5;208m'
+R = '\x1b[38;5;46m'
+F = '\x1b[38;5;48m'
+Z = '\033[1;33m'
+yellow = "\x1b[38;5;208m"
+black="\033[1;30m"
+rad="\033[1;31m"
+green="\x1b[38;5;47m"
+yelloww="\033[1;33m"
+blue="\033[1;34m"
+purple="\033[1;35m"
+cyan="\033[1;36m"
+white="\033[1;37m"
+faltu = "\033[1;41m"
+pvt = "\033[1;0m"
+my_color = [white,blue,green]
+warna = random.choice(my_color)
+
+loop=0
+oks=[]
+cps=[]
+pcp=[]
+id=[]
+tokenku=[]
+#+++++++++++M1-USERAGENT+++++++++++#
+def M3():
+    ua = f"[FBAN/FB4A;FBAV/"+str(random.randint(10, 99))+".0.0."+str(random.randint(1000, 9999))+";[FBAN/FB4A;FBAV/79.0.0.18.71;FBBV/31009779;FBDM/{density=1.5,width=540,height=960};FBLC/en_US;FBCR/Robi;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G531H;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]"
+    return ua
+
+
+
+def M2():
+    ua = f"[FBAN/FB4A;FBAV/"+str(random.randint(10, 99))+".0.0."+str(random.randint(1000, 9999))+";[FBAN/FB4A;FBAV/76.0.0.24.67;FBBV/29581438;FBDM/{density=1.5,width=540,height=960};FBLC/en_US;FBCR/Robi;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G531H;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]"
+    return ua
+
+
+def M1():
+    ua = f"[FBAN/FB4A;FBAV/"+str(random.randint(10, 99))+".0.0."+str(random.randint(1000, 9999))+";[FBAN/FB4A;FBAV/75.0.0.23.69;FBBV/29142836;FBDM/{density=1.5,width=540,height=960};FBLC/en_GB;FBCR/Robi;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-J200F;FBSV/5.1.1;FBOP/19;FBCA/armeabi-v7a:armeabi;]"
+    return ua
+
+    
+    
+    
+sys.stdout.write('\x1b]2; =[🌺]=[TEAM-69]=[🌺]=\x07')
+S = '\033[1;97m'
+A = '\x1b[38;5;208m'
+R = '\x1b[38;5;46m'
+F = ' '
+Z = '\033[1;33m'
+head = {'Host': 'adsmanager.facebook.com', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'viewport-width': '980'}
+#+++++++++++LOGO+++++++++++#
+logo=(f'''
+\x1b[38;5;46m██   ██ ███████ ███    ██ ████████  █████  ██
+\x1b[38;5;46m██   ██ ██      ████   ██    ██    ██   ██ ██
+\x1b[38;5;46m███████ █████   ██ ██  ██    ██    ███████ ██
+\x1b[38;5;46m██   ██ ██      ██  ██ ██    ██    ██   ██ ██
+\x1b[38;5;46m██   ██ ███████ ██   ████    ██    ██   ██ ██ \x1b[38;5;196m TEAM 69
+\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+ \x1b[38;5;196m[\x1b[1;97m=\x1b[38;5;196m]\033[38;5;46m DEVELOPER  \x1b[1;97m● \033[38;5;46mBD NAFIS
+ \x1b[38;5;196m[\x1b[1;97m=\x1b[38;5;196m]\033[38;5;46m ADMIN    \x1b[1;97m  ● \033[38;5;46mNAFIS\033[0;37m ● \x1b[38;5;46mREYAD
+ \x1b[38;5;196m[\x1b[1;97m=\x1b[38;5;196m]\033[38;5;46m GITHUB   \x1b[1;97m  ● \033[38;5;46mTEAM-69
+ \x1b[38;5;196m[\x1b[1;97m=\x1b[38;5;196m]\033[38;5;46m TOOLS      \x1b[1;97m● \033[0;101mFILE  \033[0m
+\x1b[38;5;196m [\x1b[38;5;46m=\x1b[38;5;196m]\x1b[38;5;51m VERSION \033[1;97m   ● \033[1;97mV\x1b[38;5;46m/\033[1;97m0.11
+\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+#================OK-CP-RESULT==============#    
+def result(OKs,cps):
+    if len(OKs) != 0 or len(cps) != 0:
+        print('')
+        print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+        print('\033[1;97m[\x1b[38;5;47m≈\033[1;97m] \033[1;97mTHE PROGRESS HAS BEEN COMPLETE ')
+        print('\033[1;97m[\x1b[38;5;47m≈\033[1;97m] \033[1;97mTOTAL OK ID:\x1b[38;5;47m %s' % str(len(oks)))
+        print('\033[1;97m[\x1b[38;5;47m≈\033[1;97m] \033[1;97mTOTAL CP ID:\033[1;30m %s' % str(len(cps)))
+        print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+        input("Press enter to back NAFIS Menu ")
+        exit()
+#++++++++++++++++++MAIN-MENU++++++++++++++#
+def NAFIS():   
+    os.system('clear')
+    print(logo)
+    print("\x1b[38;5;196m[\x1b[38;5;49m1\x1b[38;5;196m]\x1b[38;5;46m FILE CLONING\033[1;37m")
+    print("\x1b[38;5;196m[\x1b[38;5;49m2\x1b[38;5;196m]\x1b[38;5;48m CONTACT ADMIN\033[1;37m")
+    print("\x1b[38;5;196m[\x1b[38;5;49m0\x1b[38;5;196m]\x1b[38;5;49m EXIT")
+    print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+    select = input('Select Menu :\033[1;35m ')
+    if select =='1':
+        method_crack()
+    elif select =='2':
+        os.system(f'xdg-open https://m.me/j/AbZzndZFDLI6K7Dr/')
+        NAFIS()        
+    elif select =='0':
+        exit()
+    else:
+        print('\x1b[38;5;46m Select Valid Option ...')
+    time.sleep(2)
+#+++++++++++++++METHOD-MENU++++++++++++#        
+def method_crack():
+    global methods
+    os.system('clear')
+    print(logo)
+    print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m METHOD  ❯ 1💚')
+    print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m METHOD show cookies ❯ 2💚')
+    print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m METHOD  CP OFF❯ 3💚')
+    print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+    option = input('Select method:\033[1;97m ')
+    if option =='1':
+        methods.append('methodA')
+        main_crack().crack(id)
+    elif option =='2':
+        methods.append('methodB')
+        main_crack().crack(id)
+    elif option =='3':
+        methods.append('methodC')
+        main_crack().crack(id)
+    else:
+      print('\n Select Valid Option ...')
+      time.sleep(2)
+      method_crack()
+#+++++++++FILE-PATH+++++++++#
+class main_crack():
+    def __init__(self):
+        self.id=[]
+    def crack(self,id):
+        global methods
+        os.system('clear')
+        print(logo)
+        self.file = input('\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m ENTER FILE PATH :\x1b[38;5;47m ')
+        try:
+            self.id = open(self.file).read().splitlines()
+            self.pasw()
+        except FileNotFoundError:
+            print('Opps File Not Found ...')
+            time.sleep(2)
+            os.system('clear')
+            print(logo)
+            print('Try Again ...')
+            time.sleep(2)
+            main_crack().crack(id)
+#===============M1 METHOD==============#            
+    
+
+
+#++++++++++++±+++++METHOD-PASW-MENU+++++++++++++++++#            
+    def pasw(self):       
+            pw = []
+            os.system('clear')
+            print(logo)
+            sl = int(input('\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m ENTER PASSWORD LIMIT : \033[1;97m'))
+            os.system("clear")
+            print(logo)
+            print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m] \x1b[38;5;46mEXAMPLE \x1b[1;97m● \x1b[38;5;46mfirst last \x1b[38;5;196m|\x1b[38;5;46m first123')
+            print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m] \x1b[38;5;46mEXAMPLE \x1b[1;97m● \x1b[38;5;46m57273200 59039200 57575751 ') 
+            print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+            if sl =='':
+                print('\n Put limit between 1 to 30')
+            elif sl > 30:
+                print('\nPassword limit Should Not Be Greater Than 30')
+            else:
+                for sr in range(sl):
+                    pw.append(input(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m ENTER PASSWORD {sr+1}\033[1;97m : \033[1;33m'))
+                    print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+            os.system("clear")
+            print(logo)
+            print('\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;47m TOTAL ACCOUNT\033[1;97m :\x1b[38;5;47m %s ' % len(self.id))
+            print(f"\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;48m LIMIT OF PASS \033[1;97m:\x1b[38;5;48m {sl}\033[1;37m")
+            print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m] \033[38;5;49mIF NO RESULT [\x1b[38;5;46mON\033[1;37m/\033[1;33mOFF\x1b[38;5;49m] \x1b[38;5;49mAIRPLAN MODE')
+            print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+            with NAFISNAFIS(max_workers=30) as NAFISworld:
+                for zsb in self.id:
+                   try:
+                       uid, name = zsb.split('|')
+                       sz = name.split(' ')
+                       if len(sz) == 3 or len(sz) == 4 or len(sz) == 5 or len(sz) == 8:
+                           pwx =  pw
+                       else:
+                            pwx =  pw
+                            if 'methodA' in methods:
+                                NAFISworld.submit(self.methodA, uid, name, pwx)
+                            elif 'methodB' in methods:
+                                NAFISworld.submit(self.methodB, uid, name, pwx)
+                            elif 'methodC' in methods:
+                                NAFISworld.submit(self.methodC, uid, name, pwx)
+                            elif 'methodD' in methods:
+                                NAFISworld.submit(self.methodD, uid, name, pwx)    
+                   except:pass
+            result(oks,cps)
+#++++++++LICENCES++++++++#
+def NAFISALWAYSONFIRE():
+  uuid = str(os.geteuid()) + str(os.getlogin())
+  id = "`".join(uuid)
+  server = requests.get(f'https://github.com/T-EAM69-Hentai/Appeare/blob/main/Appeare.txt').text
+  
+ 
+
+  os.system(f" clear")                          
+  print(f"""\x1b[1;97m
+\x1b[38;5;46m██   ██ ███████ ███    ██ ████████  █████  ██
+\x1b[38;5;46m██   ██ ██      ████   ██    ██    ██   ██ ██
+\x1b[38;5;46m███████ █████   ██ ██  ██    ██    ███████ ██
+\x1b[38;5;46m██   ██ ██      ██  ██ ██    ██    ██   ██ ██
+\x1b[38;5;46m██   ██ ███████ ██   ████    ██    ██   ██ ██ \x1b[38;5;196m TEAM 69
+\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+ \x1b[38;5;196m[\x1b[1;97m=\x1b[38;5;196m]\033[38;5;46m DEVELOPER  \x1b[1;97m● \033[38;5;46mBD NAFIS
+ \x1b[38;5;196m[\x1b[1;97m=\x1b[38;5;196m]\033[38;5;46m ADMIN    \x1b[1;97m  ● \033[38;5;46mNAFIS\033[0;37m ● \x1b[38;5;46mREYAD
+ \x1b[38;5;196m[\x1b[1;97m=\x1b[38;5;196m]\033[38;5;46m GITHUB   \x1b[1;97m  ● \033[38;5;46mTEAM-69
+ \x1b[38;5;196m[\x1b[1;97m=\x1b[38;5;196m]\033[38;5;46m TOOLS      \x1b[1;97m● \033[0;101mFILE  \033[0m
+\x1b[38;5;196m [\x1b[38;5;46m=\x1b[38;5;196m]\x1b[38;5;51m VERSION \033[1;97m   ● \033[1;97mV\x1b[38;5;46m/\033[1;97m0.11
+\033[1;31m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\033[1;37m""")           
+  print(f"Your Key : \033[1;32m"+id)                                
+  print(f"\033[1;37mThis Tool Is Free But You Need To Access This Tool")
+  print(f"Send Your Key WhatsApp")
+  #os.system('xdg-open https://wa.me/+8801861367215')      
+  time.sleep(1)
+  print(f"\033[1;37mChecking Your Key")
+  try:
+    httpCaht = requests.get(f"https://github.com/T-EAM69-Hentai/Appeare/blob/main/Appeare.txt").text
+    if id in httpCaht:
+      print(f"\033[1;32mCongratulations ! Your Key Is Approved");time.sleep(2)
+      msg = str(os.geteuid())
+      time.sleep(0.5)
+      pass
+    else:      
+      print(f"This Tool Is Free But You Need To Access This Tool")
+      print(f"Your Key Not Approved")
+      print(f"Send Key For Access This Tool"); time.sleep(1)
+      os.system(f'xdg-open {tan}://wa.me/+8801861367215?text='+id)
+      time.sleep(1)
+      sys.exit()
+  except:
+    sys.exit()
+    if name == '__main__':
+    	print(logo)
+    	NAFISALWAYSONFIRE()
+NAFISALWAYSONFIRE()
+        
+#++++++++++++++APPROVAL+++++++++++++++#
